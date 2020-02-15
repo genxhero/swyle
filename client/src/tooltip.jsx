5d60208685a16fac9c9f42b989c03c373b8fdaaa
genxhero <genxhero@users.noreply.github.com>
2020-02-14 22:06:28 -0800

    add the new class names, clean up the tabbing

--- client/src/tooltip.jsx
+++ client/src/tooltip.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 
+/**
+ * Expected Props:
+ *    message: String, text to be displayed.
+ *    visibility: Boolean, whether the component can be seen.
+ */
+
 const Tooltip = props => {
     const {message, visibility} = props;
     console.log(message, visibility)
