b87ea82a2c24222457fb59b9524202eccf70098a
genxhero <genxhero@users.noreply.github.com>
2020-02-16 08:46:17 -0800

    remove console from tooltip

--- client/src/tooltip.jsx
+++ client/src/tooltip.jsx
@@ -8,7 +8,6 @@ import React from 'react';
 
 const Tooltip = props => {
     const {message, visibility} = props;
-    console.log(message, visibility)
     return (
         <span className={`tooltip ${visibility === true ? 'visible' : 'hidden'}`}>
             <div className="tooltip-arrow" /><span>{message}</span>
