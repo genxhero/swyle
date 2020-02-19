0caf6e8fa2899b1e9251dff4388b2aa9e2c54ad8
genxhero <genxhero@users.noreply.github.com>
2020-02-19 07:26:52 -0800

    remove debugger

--- client/src/article_subscription.jsx
+++ client/src/article_subscription.jsx
@@ -12,7 +12,6 @@ const Subscription = ({ subscribeToMore }) => {
         return subscribeToMore({
             document: ArticleSubscription,
             updateQuery: (prev, { subscriptionData }) => {
-                debugger;
                 if (!subscriptionData.data) return prev;
                
                 // const { articleLiked } = subscriptionData.data;
