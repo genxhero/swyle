3afc87e5ac8e88f9ce7e678dc6a350b79a383fd5
genxhero <genxhero@users.noreply.github.com>
2020-03-02 22:23:13 -0800

    add link to markdown guide

--- client/src/article_create.jsx
+++ client/src/article_create.jsx
@@ -55,7 +55,7 @@ class ArticleCreate extends Component {
                 <form className={`article-create-form ${this.props.colorScheme || 'classic'}`} onSubmit={this.save}>
                     <span className="post-creation-label">Article Title</span>
                     <input className={`article-create-title ${this.props.colorScheme || 'classic'}`} type="text" onChange={this.handleFormChange("title")}  value={this.state.title} />
-                    <span className="post-creation-label">Article Body</span>
+                    <span className="post-creation-label">Article Body - <a href="https://www.markdownguide.org/basic-syntax">Markdown</a> Now Supported</span>
                     <textarea className={`article-create-body ${this.props.colorScheme || 'classic'}`} type="text" onChange={this.handleFormChange("body")} value={this.state.description} />
                     <input type="submit" className={`submit ${this.props.colorScheme}`} value="Post Article" disabled={!this.state.body} />
                 </form>
