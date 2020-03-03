ba8ca9eb81de34b6bb5a046bf6c94f02f4b0eb2a
genxhero <genxhero@users.noreply.github.com>
2020-03-02 22:35:45 -0800

    preventdefault

--- client/src/formatting_buttons.jsx
+++ client/src/formatting_buttons.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 
 const FormattingButtons = props => {
-    const boldTag = () => {
+    const boldTag = (e) => {
+        e.preventDefault();
         let t = (document.all) ? document.selection.createRange().text : document.getSelection();
         debugger;
     }
