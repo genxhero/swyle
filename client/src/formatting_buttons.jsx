3b74cf9a82974372294f76b348d367a797c8afd5
genxhero <genxhero@users.noreply.github.com>
2020-03-04 06:48:42 -0800

    delete superfluous functions made redundant by tagWrap

--- client/src/formatting_buttons.jsx
+++ client/src/formatting_buttons.jsx
@@ -9,36 +9,6 @@ import PropTypes from 'prop-types'
  */
 const FormattingButtons = props => {
 
-    /**
-     * TODO: Make dry, more dynamic. 
-     */
-    const boldTag = (e) => {
-         e.preventDefault();
-        //  let selection = (document.all) ? document.selection.createRange().text : document.getSelection().toString();
-         const input = document.getElementById(props.elementId);
-         const text = input.value;
-         const start = input.selectionStart;
-         const finish = input.selectionEnd;
-         const sel = text.substring(start, finish);
-
-         const  newText = text.substring(0, start) + "**" + sel + "**" + text.substring(finish, text.length);
-         input.value = newText;
-         props.updateState(newText)
-    }
-
-    const italicTag = (e) => {
-        e.preventDefault();
-        // let selection = (document.all) ? document.selection.createRange().text : document.getSelection().toString();
-        const input = document.getElementById(props.elementId);
-        const text = input.value;
-        const start = input.selectionStart;
-        const finish = input.selectionEnd;
-        const sel = text.substring(start, finish);
-        const newText = text.substring(0, start) + "*" + sel + "*" + text.substring(finish, text.length);
-        input.value = newText;
-        props.updateState(newText)
-    }
-
     const tagWrap = (e) => {
         e.preventDefault();
         const input = document.getElementById(props.elementId);
