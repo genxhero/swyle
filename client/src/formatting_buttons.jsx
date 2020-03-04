cdcc9ceb5c0c72dc618ea04c85eb618141f48441
genxhero <genxhero@users.noreply.github.com>
2020-03-03 23:23:43 -0800

    get markdown tag adders to work

--- client/src/formatting_buttons.jsx
+++ client/src/formatting_buttons.jsx
@@ -5,6 +5,7 @@ import $ from 'jquery';
  * 
  * Only finds the first instance. Hm. Gonna need to find the index of the substring I reckon.
  * Also doesn't update state, so need to update state.
+ * Also need to get rid of trailing spaces.
  */
 const FormattingButtons = props => {
 
@@ -13,13 +14,31 @@ const FormattingButtons = props => {
          let selection = (document.all) ? document.selection.createRange().text : document.getSelection().toString();
          const input = document.getElementById(props.elementId);
          const text = input.value;
-         input.value = text.replace(selection, `**${selection}**`);
-         props.updateState(text.replace(selection, `**${selection}**`))
+         const start = input.selectionStart;
+         const  finish = input.selectionEnd;
+         const sel = text.substring(start, finish);
+         const  newText = text.substring(0, start) + "**" + sel + "**" + text.substring(finish, text.length);
+         input.value = newText;
+         props.updateState(newText)
+    }
+
+    const italicTag = (e) => {
+        e.preventDefault();
+        let selection = (document.all) ? document.selection.createRange().text : document.getSelection().toString();
+        const input = document.getElementById(props.elementId);
+        const text = input.value;
+        const start = input.selectionStart;
+        const finish = input.selectionEnd;
+        const sel = text.substring(start, finish);
+        const newText = text.substring(0, start) + "*" + sel + "*" + text.substring(finish, text.length);
+        input.value = newText;
+        props.updateState(newText)
     }
 
     return (
         <div className="formatting-section">
-            <button className="formatting-button" name="B" onClick={boldTag} />
+            <button className="formatting-button" value="B" name="B" onClick={boldTag}>B</button>
+            <button className="formatting-button" value="I" name="I" onClick={italicTag}>I</button>
         </div>
     )
 }
