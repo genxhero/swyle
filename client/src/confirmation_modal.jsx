8f685eeb20b7c65df610551901a90e75efd357df
genxhero <genxhero@users.noreply.github.com>
2020-03-08 23:39:18 -0700

    improve confirmation modal

--- client/src/confirmation_modal.jsx
+++ client/src/confirmation_modal.jsx
@@ -4,6 +4,7 @@ import {MdCheck, MdClose} from 'react-icons/md';
 /**
  * Modal which displays upon an attempt to delete an article or image post.
  * 
+ * TODO: Figure out whether tis better to import the newCloseModal directly into this component and ONLY pass the callback.
  * Expected Props
  *      title: String - the name of the thing to be deleted
  *      confirm: Function - should make a DELETE request to one's backend; or it could even be used 
@@ -14,13 +15,14 @@ import {MdCheck, MdClose} from 'react-icons/md';
  */
 
 const ConfirmationModal = (props) => {
+    const callback = props.callback ? props.callback :  () => {  return 0 }
     return (
         <div className="confirmation-modal">
             <div className="confirmation-dialog">
                 <h4>Are you sure you want to delete <span>"{props.title}"</span>?</h4>
                 <div className="confirmation-icons" style={{"display": "flex"}}>     
                 <MdCheck className="confirmation-yes" onClick={props.confirm} />
-                <MdClose className="confirmation-no" onClick={() => props.cancel(props.callback)} />
+                <MdClose className="confirmation-no" onClick={() => props.cancel(callback)} />
 
                 </div>
            
