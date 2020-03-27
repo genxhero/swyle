bb9a3c8e10254f42532124575c10ad4e241ad9d7
genxhero <genxhero@users.noreply.github.com>
2020-03-27 06:23:23 -0700

    destructure the props properly

--- client/src/article_card.jsx
+++ client/src/article_card.jsx
@@ -3,6 +3,7 @@ import ArticleTags from './article_tags';
 import ReactMarkdown from 'react-markdown';
 
 const ArticleCard = props => {
+    const {date, colorScheme, article } = props;
     return (
         <div className={`article-index-card ${colorScheme}`} key={`${article.id}${article.title}${date}`}>
             <Link className={`article-index-show-link ${colorScheme}`} to={`/articles/${article.id}`}>
