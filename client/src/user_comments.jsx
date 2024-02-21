55c655f6da3b2bb33f1209ca0407d04678365a9e
Aaron Goddard <36572308+genxhero@users.noreply.github.com>
2024-02-21 11:27:20 -0800

    refactor user comments to use@apollo

--- client/src/user_comments.jsx
+++ client/src/user_comments.jsx
@@ -1,4 +1,45 @@
 import React from 'react';
+import { useQuery } from '@apollo/client';
+import commentsByUser from './queries/comments_by_user';
+
+/**
+ * Displays recent comments by the current user
+ * 
+ * colorScheme: String, color scheme chosen by the user
+ * userId: Integer, primary key for the users table 
+ */
+const UserComments = (props) => {
+    const { loading, error, data } = useQuery(commentsByUser, {
+        variables: { userId: props.userId },
+    });
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error</p>;
+
+    return (
+        <div className={`user-comments user-comments-${props.colorScheme}`}>
+            <div>
+                {data.commentsByUser.map(comment => (
+                    <div className={`user-comments-card user-comments-card-${props.colorScheme}`} key={comment.id}>
+                        <h4>You commented:</h4>
+                        <p className={`user-comments-card-body user-comments-card-body-${props.colorScheme}`}>{comment.body}</p>
+                        <h5>
+                            <span>
+                                On <span>{comment.post.title}</span> by <span>{comment.post.author.username}</span>
+                                <span> at {comment.createdAt}</span>
+                            </span>
+                        </h5>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default UserComments;
+
+/**
+import React from 'react';
 import commentsByUser from './queries/comments_by_user';
 import { Query } from 'react-apollo';
 
@@ -8,7 +49,7 @@ import { Query } from 'react-apollo';
  * 
  * colorScheme: String, color scheme chosen by the user
  * userId: Integer, primary key for the users table 
- */
+ 
 const UserComments = (props) => {
     return (
         <div className={`user-comments user-comments-${props.colorScheme}`}>
@@ -39,3 +80,4 @@ const UserComments = (props) => {
 }
 
 export default UserComments;
+*/
