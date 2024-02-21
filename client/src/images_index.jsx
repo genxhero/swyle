40bb0042d7ff1852ed244dd82a8a0f99e08936b1
Aaron Goddard <36572308+genxhero@users.noreply.github.com>
2024-02-21 12:47:20 -0800

    Refactor images index page to be sexier and sleeker

--- client/src/images_index.jsx
+++ client/src/images_index.jsx
@@ -1,3 +1,42 @@
+import React, { useEffect } from 'react';
+import { useQuery } from 'react-apollo';
+import images from './queries/images';
+import { Link } from 'react-router-dom';
+import Subscription from './images_subscription';
+import Disclaimer from './disclaimer';
+
+const ImagesIndex = ({ colorScheme }) => {
+    const { loading, error, data, subscribeToMore } = useQuery(images);
+
+    useEffect(() => {
+        // Subscribe to more data if needed
+    }, [subscribeToMore]);
+
+    if (loading) return <div className={`loading-div loading-div-${colorScheme}`}><img className="loading-img" alt="load" src="https://i.gifer.com/origin/4d/4dc11d17f5292fd463a60aa2bbb41f6a_w200.gif" /></div>;
+    if (error) return <p>Error :(</p>;
+
+    return (
+        <div className={`image-index-page image-index-page-${colorScheme}`}>
+            <h1>Newest Images</h1>
+            <Disclaimer />
+            <div className="image-index-spread">
+                {data.images.map(image => (
+                    <div className={`image-index-card ${colorScheme}`} key={`${image.title}${image.author.username}`}>
+                        <Link to={`/images/${image.id}`}>
+                            <img className="image-index-thumb" src={image.image} alt={image.title} />
+                        </Link>
+                        <span>by {image.author.username}</span>
+                        <span>{image.count} Comments, {image.likeCount} Likes</span>
+                    </div>
+                ))}
+            </div>
+            <Subscription subscribeToMore={subscribeToMore} />
+        </div>
+    );
+};
+
+export default ImagesIndex;
+/**
 import React, { Component } from 'react';
 import images from './queries/images';
 import { Query } from "react-apollo";
@@ -5,11 +44,8 @@ import {Link} from 'react-router-dom';
 import Subscription from './images_subscription';
 import Disclaimer from './disclaimer';
 
-/**
- * Component for displaying all of the images in chronological order.  No analytics, no metrics,
- * no assumptions. Deal with it.
- * TODO: change to functional component
- */
+//Component for displaying all of the images in chronological order.  No analytics, no metrics, no assumptions. Deal with it.
+
 class ImagesIndex extends Component {
     constructor(props) {
         super(props);
@@ -54,3 +90,4 @@ class ImagesIndex extends Component {
 }
 
 export default ImagesIndex;
+*/
