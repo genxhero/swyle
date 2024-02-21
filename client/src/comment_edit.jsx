efdd41e300749c21b95f84c28cf9066186f9b97c
Aaron Goddard <36572308+genxhero@users.noreply.github.com>
2024-02-21 11:40:29 -0800

    convert comment edit to functional component

--- client/src/comment_edit.jsx
+++ client/src/comment_edit.jsx
@@ -1,12 +1,64 @@
+import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import updateComment from './mutations/update_comment';
+import article from './queries/article';
+import image from './queries/image';
+
+const CommentEdit = (props) => {
+    const [body, setBody] = useState(props.comment.body);
+
+    const refetch = props.postType === "Article" ? article : image;
+
+    const [updateCommentMutation, { loading }] = useMutation(updateComment, {
+        refetchQueries: [{ query: refetch, variables: { id: props.postId } }]
+    });
+
+    const handleFormChange = (event) => {
+        setBody(event.currentTarget.value);
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        updateCommentMutation({
+            variables: {
+                body: body,
+                id: props.comment.id,
+                postType: props.postType
+            }
+        }).then(res => {
+            props.cancelEdit();
+        });
+    };
+
+    return (
+        <div className="comment">
+            <form className="comment-edit-form" onSubmit={handleSubmit}>
+                <textarea
+                    className="comment-edit-body"
+                    value={body}
+                    onChange={handleFormChange}
+                />
+                <div className="comment-edit-buttons">
+                    <input className="confirm-btn-yes" type="submit" />
+                    <button className="confirm-btn-no" onClick={props.cancelEdit}> Cancel</button>
+                </div>
+            </form>
+        </div>
+    );
+};
+
+export default CommentEdit;
+
+/**
 import React, {Component} from 'react';
 import {Mutation} from 'react-apollo';
 import updateComment from './mutations/update_comment';
 import article from './queries/article';
 import image from './queries/image';
 
-/**
- * Component form for editing a user comment.  Refetches the query for which ever post it belongs to in order to update the dom
- */
+
+ // Component form for editing a user comment.  Refetches the query for which ever post it belongs to in order to update the dom
+ 
 
 class CommentEdit extends Component {
     constructor(props) {
@@ -62,4 +114,4 @@ class CommentEdit extends Component {
     }
 }
 
-export default CommentEdit;
+export default CommentEdit; */
