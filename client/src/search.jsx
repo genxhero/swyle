7a84c62e2e7c276765a552a7ad714f98cafdaca0
Aaron Goddard <36572308+genxhero@users.noreply.github.com>
2024-02-21 11:22:32 -0800

    Migrate Search to @apollo/client

--- client/src/search.jsx
+++ client/src/search.jsx
@@ -1,3 +1,47 @@
+import React from 'react';
+import { useQuery } from '@apollo/client';
+import { Link } from 'react-router-dom';
+import search from './queries/posts_by_query';
+import { parseUrl } from './helpers';
+import Subscription from './search_subscription';
+
+const Search = (props) => {
+    const query = props.location.search;
+
+    const { loading, error, data, subscribeToMore } = useQuery(search, {
+        variables: { searchQuery: query },
+    });
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error :(</p>;
+    if (data.postsByQuery.length === 0) {
+        return <h1 style={{ "color": "white", "margin": "2rem" }}>Sorry, no posts matched your query</h1>;
+    }
+
+    return (
+        <div>
+            <div className="search-results-spread">
+                {data.postsByQuery.map(post => {
+                    const url = parseUrl(post.__typename);
+                    return (
+                        <div className="user-posts-card" key={`${post.title}${post.title.length}${post.id}`}>
+                            <h3><Link to={`${url}/${post.id}`}>{post.title}</Link></h3>
+                            {post.image && <div className="user-posts-thumbnail-container"><img src={post.image} alt={post.image.title} /></div>}
+                            {post.snippet && <p>{post.snippet}<span style={{ "color": "gray" }}>...</span></p>}
+                        </div>
+                    );
+                })}
+                <Subscription subscribeToMore={subscribeToMore} query={query.replace("?q=", "")} />
+            </div>
+        </div>
+    );
+};
+
+export default Search;
+
+
+//old code below
+/**
 import React from 'react'
 import { withApollo, Query } from 'react-apollo'
 import search from './queries/posts_by_query';
@@ -11,7 +55,7 @@ import Subscription from './search_subscription';
  * 
  * @param {*} props 
  */
-
+/**
 const Search = (props) =>  {
         const query = props.location.search;
 
@@ -44,3 +88,4 @@ const Search = (props) =>  {
 }
 
 export default withApollo(Search);
+*/
