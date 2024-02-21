98597b0f474673caf60a9089ec5c91ee7f676e5c
Aaron Goddard <36572308+genxhero@users.noreply.github.com>
2024-02-21 11:14:19 -0800

    Prepare App.js for full conversion to hooks

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,9 @@
  */
 import './stylesheets/App.scss';
 import React, { Component } from 'react';
-import { ApolloProvider } from "react-apollo"
+//import { ApolloProvider } from "react-apollo"
+//import {Query} from 'react-apollo';
+import { ApolloProvider, useQuery } from '@apollo/client';
 import ArticlesIndex from './articles_index';
 import ImagesIndex from './images_index';
 import ImageCreate from './image_create';
@@ -21,7 +23,6 @@ import Adverts from './adverts';
 import { Switch, Route } from 'react-router-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { createHashHistory } from 'history';
-import {Query} from 'react-apollo';
 import currentUser from './queries/current_user';
 import { createCache, createClient } from './utils/apollo';
 
@@ -34,6 +35,41 @@ const testArea = () => {
   return <div />
 }
 
+const App = ({ client }) => {
+  const { data, loading } = useQuery(currentUser);
+
+  if (loading) return null;
+
+  const colorScheme = data?.currentUser?.colorScheme || "standard";
+
+  return (
+    <BrowserRouter>
+      <ApolloProvider client={client}>
+        <div>
+          <Header currentUser={data.currentUser} client={client} colorScheme={colorScheme} />
+          <Switch>
+            <Route exact path="/" render={(props) => <ArticlesIndex colorScheme={colorScheme} {...props}/> } />
+            <Route exact path="/articles/new" render={(props) => <ArticleCreate colorScheme={colorScheme} {...props} />}/>
+            <Route exact path="/articles/:articleID" render={(props) => <ArticleShow colorScheme={colorScheme} currentUser={data.currentUser} {...props}/>} />
+            <Route exact path="/images/new" render={(props) => <ImageCreate colorScheme={colorScheme} {...props}/>} />
+            <Route exact path="/images/:imageID" render={(props) => <ImageShow colorScheme={colorScheme} {...props}/>} />
+            <Route exact path="/images" render={(props) => <ImagesIndex colorScheme={colorScheme} {...props}/>} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/dashboard" render={(props) => <UserDash currentUser={data.currentUser} colorScheme={colorScheme} {...props}/>} />
+            <Route exact path="/trending" render={(props) => <Trending colorScheme={colorScheme} {...props}/>} />
+            <Route exact path="/headertest" component={testArea}/>
+            <Route exact path="/about" render={(props) => <About colorScheme={colorScheme} {...props}/>} />
+            <Route path="/search" component={Search} />
+            <Route exact path="/sponsors" component={Adverts} />
+          </Switch>
+        </div>
+      </ApolloProvider>
+    </BrowserRouter>
+  );
+};
+
+/*
 class App extends Component {
 
   render() {   
@@ -80,5 +116,5 @@ class App extends Component {
   }
 }
 
-
+*/
 export default App;
