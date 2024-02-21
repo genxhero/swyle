d4cf49419ef5ce88140efbf595c116737d801c83
Aaron Goddard <36572308+genxhero@users.noreply.github.com>
2024-02-21 13:58:37 -0800

    refactor article creation

--- client/src/article_create.jsx
+++ client/src/article_create.jsx
@@ -1,9 +1,64 @@
-/** 
- * Renders form for creating a new article
- * 
- * TODO: Assess whether i need the current user query here as i may be able to pass current user in from props
- */
 
+import React, { useState } from 'react';
+import { useMutation, useQuery } from '@apollo/client';
+import postArticleMutation from './mutations/post_article';
+import currentUserQuery from './queries/current_user';
+import articlesQuery from './queries/articles';
+import { Redirect } from 'react-router-dom';
+import FormattingButtons from './formatting_buttons';
+
+const ArticleCreate = (props) => {
+    const [title, setTitle] = useState("");
+    const [body, setBody] = useState("");
+    const { loading, data } = useQuery(currentUserQuery);
+    const [postArticle] = useMutation(postArticleMutation, {
+        refetchQueries: [{ query: articlesQuery }]
+    });
+
+    const handleFormChange = (field) => {
+        return (event) => {
+            if (field === 'title') {
+                setTitle(event.currentTarget.value);
+            } else {
+                setBody(event.currentTarget.value);
+            }
+        };
+    };
+
+    const save = async (e) => {
+        e.preventDefault();
+        postArticle({
+            variables: { title: title, body: body }
+        }).then(res => {
+            props.history.push(`/articles/${res.data.createArticle.id}`);
+        });
+    };
+
+    if (loading) {
+        return <div><img alt="loady" src="https://media3.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif?cid=790b7611672a658dd9b96b70df5cfe96a2bf98f81e8ab339&rid=giphy.gif" /></div>;
+    }
+
+    if (!data.currentUser) {
+        return <Redirect to="/login" />;
+    }
+
+    return (
+        <div className={`article-create-page ${props.colorScheme || 'classic'}`}>
+            <form className={`article-create-form ${props.colorScheme || 'classic'}`} onSubmit={save}>
+                <span className="post-creation-label">Article Title</span>
+                <input className={`article-create-title ${props.colorScheme || 'classic'}`} type="text" onChange={handleFormChange("title")} value={title} />
+                <span className="post-creation-label">Article Body - <a href="https://www.markdownguide.org/basic-syntax">Markdown</a> Now Supported</span>
+                <FormattingButtons elementId={"new-article-text"} updateState={(val) => { setBody(val) }} />
+                <textarea id="new-article-text" className={`article-create-body ${props.colorScheme || 'classic'}`} type="text" onChange={handleFormChange("body")} value={body} />
+                <input type="submit" className={`submit ${props.colorScheme}`} value="Post Article" disabled={!body} />
+            </form>
+        </div>
+    );
+};
+
+export default ArticleCreate;
+
+/**
 import React, { Component } from 'react';
 import postArticle from './mutations/post_article';
 import currentUser from './queries/current_user';
@@ -74,4 +129,4 @@ export default compose(
     graphql(currentUser)
 )(
     graphql(postArticle)(ArticleCreate)
-)
+)  */
