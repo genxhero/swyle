987d17793cf3477d64fd162e50292f8faf361899
Aaron Goddard <36572308+genxhero@users.noreply.github.com>
2024-02-21 12:42:03 -0800

    refactor title edit

--- client/src/article_title_edit.jsx
+++ client/src/article_title_edit.jsx
@@ -1,12 +1,56 @@
+import React, { useState } from 'react';
+import { useMutation } from 'react-apollo';
+import updateArticleTitle from './mutations/update_article_title';
+import article from './queries/article';
+
+const ArticleTitleEdit = (props) => {
+    const [title, setTitle] = useState(props.title);
+
+    const [updateArticleTitleMutation, { loading }] = useMutation(updateArticleTitle);
+
+    const handleFormChange = (event) => {
+        setTitle(event.target.value);
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        updateArticleTitleMutation({
+            variables: {
+                title: title,
+                id: props.id
+            },
+            refetchQueries: [{ query: article, variables: { id: props.id } }]
+        }).then(res => {
+            props.finishEdit("Title");
+        });
+    };
+
+    return (
+        <div className="edit-title">
+            {!loading ? (
+                "..."
+            ) : (
+                <form className="edit-title" onSubmit={handleSubmit}>
+                    <input className="edit-title-text" type="text" value={title} onChange={handleFormChange} />
+                    <span className="edit-save-or-cancel">
+                        <input type="submit" value="Save" className="confirm-btn-yes" name="Save" />
+                        <button onClick={props.cancelEdit} className="confirm-btn-no" name="Title">Cancel</button>
+                    </span>
+                </form>
+            )}
+        </div>
+    );
+};
+
+export default ArticleTitleEdit;
+
+/**
 import React, { Component } from 'react';
 import {Mutation} from 'react-apollo';
 import updateArticleTitle from './mutations/update_article_title'
 import article from './queries/article';
 
-/**
- * Component for editing the title of an article.
- * TODO: Merge with image_title_edit, conditionally mutate based on post type
- */
+
 class ArticleTitleEdit extends Component {
     constructor(props) {
         super(props)
@@ -61,3 +105,4 @@ class ArticleTitleEdit extends Component {
 }
 
 export default ArticleTitleEdit;
+*/
